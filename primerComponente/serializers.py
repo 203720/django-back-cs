05741add3531503dc0d04adf635841999c97b238
Azul Briones <azul.briones.09@gmail.com>
2022-01-28 22:27:26 -0600

    Add views class service

--- primerComponente/serializers.py
+++ primerComponente/serializers.py
@@ -7,4 +7,4 @@ from primerComponente.models import PrimerTabla
 class PrimerTablaSerializer(serializers.ModelSerializer):
     class Meta:
         model = PrimerTabla
-        fields= ('nombre','edad') 
+        fields = ('__all__')
