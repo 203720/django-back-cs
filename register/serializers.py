be2271b30e0654fd7cafa4a38906118b0def3ab8
203720 <203720@ids.upchiapas.edu.mx>
2022-02-26 12:30:04 -0600

    Register with validation

--- register/serializers.py
+++ register/serializers.py
@@ -1,24 +1,66 @@
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
+
+# class UserSerializer(serializers.Serializer):
+#     id = serializers.ReadOnlyField()
+#     username = serializers.CharField()
+#     email = serializers.EmailField()
+#     password = serializers.CharField()
+
+#     def create(self, validate_data):
+#         instance = User()
+#         instance.username = validate_data.get('username')
+#         instance.email = validate_data.get('email')
+#         instance.set_password(validate_data.get('password'))
+#         instance.save()
+        
+#         return instance
+
+#     def validate_username(self, data):
+#         users = User.objects.filter(username = data)
+#         if len(users) !=0:
+#             raise serializers.ValidationError("Este nombre de usuario ya existe")
+#         else:
+#             return data
+
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    password = serializers.CharField()
-
-    def create(self, validate_data):
-        instance = User()
-        instance.username = validate_data.get('username')
-        instance.email = validate_data.get('email')
-        instance.set_password(validate_data.get('password'))
-        instance.save()
-        
-        return instance
-
-    def validate_username(self, data):
-        users = User.objects.filter(username = data)
-        if len(users) !=0:
-            raise serializers.ValidationError("Este nombre de usuario ya existe")
-        else:
-            return data
+class RegisterSerializer(serializers.ModelSerializer):
+   email = serializers.EmailField(
+           required=True,
+           validators=[UniqueValidator(queryset=User.objects.all())]
+           )
+ 
+   password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+   password2 = serializers.CharField(write_only=True, required=True)
+ 
+   class Meta:
+       model = User
+       fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+       extra_kwargs = {
+           'first_name': {'required': True},
+           'last_name': {'required': True}
+       }
+ 
+   def validate(self, attrs):
+       if attrs['password'] != attrs['password2']:
+           raise serializers.ValidationError({"password": "Password fields didn't match."})
+ 
+       return attrs
+ 
+   def create(self, validated_data):
+       user = User.objects.create(
+           username=validated_data['username'],
+           email=validated_data['email'],
+           first_name=validated_data['first_name'],
+           last_name=validated_data['last_name']
+       )
+ 
+      
+       user.set_password(validated_data['password'])
+       user.save()
+ 
+       return user
--- register/views.py
+++ register/views.py
@@ -1,16 +1,24 @@
-from rest_framework.response import Response
-from .serializers import UserSerializer
-from rest_framework import status
-from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .serializers import UserSerializer
+# from rest_framework import status
+# from rest_framework import APIView
 
-class UserRegister(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+class UserRegister(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+    # def post(self, request):
+    #     serializer = UserSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         user = serializer.save()
             
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        else:
+    #     else:
             
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
